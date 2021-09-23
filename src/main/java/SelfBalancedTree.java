0f5517cc3575f5ebdb0d13f94ea27177798338d4
Myneni Mahesh <myneni.nani@gmail.com>
2021-09-23 17:24:46 +0530

    ignore

--- src/main/java/SelfBalancedTree.java
+++ src/main/java/SelfBalancedTree.java
@@ -1,26 +1,50 @@
 public class SelfBalancedTree {
     public static void main(String[] args) {
-
+/*                    30
+           20                 40
+               25
+            24     28
+         22           29    */
+        AVLTree avlTree = new AVLTree();
+        avlTree.add(30);
+        avlTree.add(40);
+        avlTree.add(20);
+        avlTree.add(25);
+        avlTree.add(28);
+        avlTree.add(24);
+        avlTree.add(29);
+        avlTree.add(22);
+        System.out.println(avlTree.height());
     }
 }
 
 class TreeNode {
-    int data;
-    int balanceFactor;
-    TreeNode left;
-    TreeNode right;
+    private int data;
+    private int leftHeight, rightHeight;
+    private TreeNode left;
+    private TreeNode right;
 
-    public TreeNode(int data, int balanceFactor, TreeNode left, TreeNode right) {
+    public TreeNode(int data,TreeNode left, TreeNode right) {
         this.data = data;
-        this.balanceFactor = balanceFactor;
         this.left = left;
         this.right = right;
     }
 
-    public TreeNode(int data, TreeNode left, TreeNode right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
+
+    public int getLeftHeight() {
+        return leftHeight;
+    }
+
+    public void setLeftHeight(int leftHeight) {
+        this.leftHeight = leftHeight;
+    }
+
+    public int getRightHeight() {
+        return rightHeight;
+    }
+
+    public void setRightHeight(int rightHeight) {
+        this.rightHeight = rightHeight;
     }
 
     public int getData() {
@@ -31,13 +55,7 @@ class TreeNode {
         this.data = data;
     }
 
-    public int getBalanceFactor() {
-        return balanceFactor;
-    }
 
-    public void setBalanceFactor(int balanceFactor) {
-        this.balanceFactor = balanceFactor;
-    }
 
     public TreeNode getLeft() {
         return left;
@@ -63,25 +81,33 @@ class AVLTree {
     public void add(int value) {
         if(treeNode == null){
             treeNode = new TreeNode(value,null,null);
+            return;
         }
         addElementToLocation(value,treeNode);
     }
 
     private void addElementToLocation(int value, TreeNode treeNode) {
-        if(value > treeNode.data){
-            if(treeNode.right != null) {
-                addElementToLocation(value, treeNode.right);
+        if(value > treeNode.getData()){
+            if(treeNode.getRight() != null) {
+                addElementToLocation(value, treeNode.getRight());
+
             } else{
-                treeNode.right = new TreeNode(value,0,null,null);
+                treeNode.setRight(new TreeNode(value,null,null));
+
             }
+            treeNode.setRightHeight(Math.max(treeNode.getRight().getLeftHeight(),treeNode.getRight().getRightHeight())+1);
         } else {
-            if(treeNode.left != null) {
-                addElementToLocation(value, treeNode.left);
+            if(treeNode.getLeft() != null) {
+                addElementToLocation(value, treeNode.getLeft());
             } else {
-                treeNode.left = new TreeNode(value,0,null,null);
+                treeNode.setLeft(new TreeNode(value,null,null));
+
             }
+            treeNode.setLeftHeight(Math.max(treeNode.getLeft().getLeftHeight(),treeNode.getLeft().getRightHeight())+1);
         }
     }
+
+
 
 
     public void remove() {
@@ -89,7 +115,7 @@ class AVLTree {
     }
 
     public int height() {
-        return 0;
+        return Math.max(treeNode.getLeftHeight(),treeNode.getRightHeight());
     }
 
 
